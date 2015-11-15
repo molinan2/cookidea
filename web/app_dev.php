22a22a56082131dbfb67425ad983242164c7a270
root <root@ct301.ip-149-202-201.eu>
2015-11-15 11:14:02 +0000

    Gitignore fixes

--- web/app_dev.php
+++ web/app_dev.php
@@ -8,7 +8,6 @@ use Symfony\Component\Debug\Debug;
 //umask(0000);
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
-/**
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     && $_SERVER['HTTP_HOST'] != 'localhost'
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
@@ -17,7 +16,6 @@ if (isset($_SERVER['HTTP_CLIENT_IP'])
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
- **/
 
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 Debug::enable();
